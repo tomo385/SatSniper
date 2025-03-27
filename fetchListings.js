015f05a815d8527a331af77ee390f75387a58a57
tomo385 <159115498+tomo385@users.noreply.github.com>
2025-03-27 20:27:50 +0000

    Delete fetchListings.js

--- fetchListings.js
+++
@@ -1,6 +0,0 @@
-export async function fetchRareSatsListings() {
-  const res = await fetch("/.netlify/functions/fetch-listings");
-  if (!res.ok) throw new Error("Failed to fetch Rare Sats listings.");
-  const data = await res.json();
-  return data;
-}
